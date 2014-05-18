8411ed0ee03faa0808b82e1b680f9782c04a9bb7
Brendan Murray <brendanpmurray@gmail.com>
2014-05-18 15:25:14 +0100

    Updated initialization

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -6,4 +6,9 @@ D:\Development\Apps\testApp\platforms\android\res\drawable-mdpi\icon.png \
 D:\Development\Apps\testApp\platforms\android\res\drawable-xhdpi\icon.png \
 D:\Development\Apps\testApp\platforms\android\res\values\strings.xml \
 D:\Development\Apps\testApp\platforms\android\res\xml\config.xml \
+D:\Development\Apps\testApp\platforms\android\ant-build\res\drawable\icon.png \
+D:\Development\Apps\testApp\platforms\android\ant-build\res\drawable-hdpi\icon.png \
+D:\Development\Apps\testApp\platforms\android\ant-build\res\drawable-ldpi\icon.png \
+D:\Development\Apps\testApp\platforms\android\ant-build\res\drawable-mdpi\icon.png \
+D:\Development\Apps\testApp\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
 D:\Development\Apps\testApp\platforms\android\ant-build\AndroidManifest.xml \
